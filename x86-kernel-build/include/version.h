cd4f9180a4a2b0ccd2c6e3f54f95cecb91c74600
AlessandroSangiuliano <alex22_7@hotmail.com>
2015-03-08 14:08:16 +0100

    first commit with dity stuffs. To ble cleaned the directories structure

---
+++ x86-kernel-build/include/version.h
@@ -0,0 +1,22 @@
+\
+//							
+\
+// Autmatically generated.  Do not edit.		
+\
+//							
+\
+						
+\
+#define KERNEL_VERSION         0			
+\
+#define KERNEL_SUBVERSION      4			
+\
+#define KERNEL_SUBSUBVERSION   0			
+
+\
+#define KERNELGENDATE          SHUFFLE3(day:(120-100),month:(102-100),year:(2015-2000))			
+\
+#define __USER__               "alex@omega"				
+\
+#define __KERNELBUILDNO__      6				
+
